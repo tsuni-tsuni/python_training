9f5a4e1871d65db292b7eec01ce6ce6c97b685f0
Elizaveta Kruchinina <penvellin@gmail.com>
2023-05-19 04:07:56 +0300

    Задание №21 - Доработка - Закомментирован доп. код из теста для home page

--- test/4_compare/test_compare_home_page.py
+++ test/4_compare/test_compare_home_page.py
@@ -8,14 +8,15 @@ def test_contact_info_on_home_page(app, db):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(lastname=generate_random_string(), firstname=generate_random_string(), address=generate_random_string(), home=generate_random_string(), email=generate_random_string()))
     contacts_from_home_page = app.contact.get_contact_list()
-    contacts_from_edit_page = app.contact.get_contacts_info_from_edit_page()
     assert sorted(contacts_from_home_page, key=Contact.id_or_max) == sorted(db.get_contact_list(), key=Contact.id_or_max)
-    for i in range(len(contacts_from_home_page)):
-        assert contacts_from_home_page[i].lastname == contacts_from_edit_page[i].lastname
-        assert contacts_from_home_page[i].firstname == contacts_from_edit_page[i].firstname
-        assert contacts_from_home_page[i].address == contacts_from_edit_page[i].address
-        assert contacts_from_home_page[i].all_emails_from_home_page == merge_emails_like_on_home_page(contacts_from_edit_page[i])
-        assert contacts_from_home_page[i].all_phones_from_home_page == merge_phones_like_on_home_page(contacts_from_edit_page[i])
+    # compare home & edit pages
+    # contacts_from_edit_page = app.contact.get_contacts_info_from_edit_page()
+    # for i in range(len(contacts_from_home_page)):
+    #     assert contacts_from_home_page[i].lastname == contacts_from_edit_page[i].lastname
+    #     assert contacts_from_home_page[i].firstname == contacts_from_edit_page[i].firstname
+    #     assert contacts_from_home_page[i].address == contacts_from_edit_page[i].address
+    #     assert contacts_from_home_page[i].all_emails_from_home_page == merge_emails_like_on_home_page(contacts_from_edit_page[i])
+    #     assert contacts_from_home_page[i].all_phones_from_home_page == merge_phones_like_on_home_page(contacts_from_edit_page[i])
 
 
 def clear(s):
