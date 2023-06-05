26cef4ada23613778cfdb534d96dd8429f9207b3
Elizaveta Kruchinina <penvellin@gmail.com>
2023-06-05 03:52:15 +0300

    Редактирование теста по удалению группы на использование UI вместо DB

--- test/1_add/test_add_group.py
+++ test/1_add/test_add_group.py
@@ -3,9 +3,11 @@ from model.group import Group
 
 def test_add_group(app, db, check_ui, json_groups):
     group = json_groups
-    old_groups = db.get_group_list()
+    #old_groups = db.get_group_list()
+    old_groups = app.group.get_group_list()
     app.group.create(group)
-    new_groups = db.get_group_list()
+    #new_groups = db.get_group_list()
+    new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
